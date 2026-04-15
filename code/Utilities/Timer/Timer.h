0978c6fcfb20250522addd2aad5e8199f58cb9fc
wayubo <obwyb@qq.com>
2026-04-15 17:47:15 +0800

    解决冲突

--- code/Utilities/Timer/Timer.h
+++ code/Utilities/Timer/Timer.h
@@ -14,20 +14,49 @@ using Time_ms = std::chrono::milliseconds;
 class Timer {
    private:
     // Timer全局单例模式
-    Timer() = default;
+    Timer(): startTime(Clock::now()), timeout(Time_ms(1000)) {}
     ~Timer() = default;
+    // 记录初始化时间
+    TimePoint startTime;
+    // 超时时间（初始为一秒）
+    Time_ms timeout;
 
    public:
     // Timer全局单例模式
     Timer(const Timer&) = delete;
-    static Timer& instance();
+    Timer& operator=(const Timer&) = delete;
+
+    // 实现定时器的单例模式
+    static Timer& instance() {
+        static Timer timer;
+        return timer;
+    }
 
     // 记录程序开始时间戳
-    static inline void startTimer();
+    inline void startTimer() {
+        startTime = Clock::now();
+    }
+
     // 获得现在时间戳
-    static inline TimePoint getNowTimePoint();
+    inline TimePoint getNowTimePoint() {
+        return Clock::now();
+    }
+
     // 获得过去的时间(ms)
-    static inline int getPassedTime();
+    inline int getPassedTime() {
+        auto now = Clock::now();
+        return std::chrono::duration_cast<Time_ms>(now - startTime).count();
+    }
+
+    // 设置超时时间
+    inline void setTimeout(Time_ms timeout) {
+        this->timeout = timeout;
+    }
+
+    // 检查是否超时
+    inline bool isTimeout() {
+        return getPassedTime() >= timeout.count();
+    }
 };
 }  // namespace VanitasBot::Utilities
 #endif
