3ddc64c1bbc03d8caa2800edd90e5535cd4b2c3c
V.Reason <2665656831@qq.com>
2026-04-18 22:41:03 +0800

    feat: 完成全部测试，进入初代封装阶段

--- code/main.cpp
+++ code/main.cpp
@@ -53,8 +53,14 @@ int main() {
 
     BitEngine::BitBoard board;
     IOEngine::initBoard(board);
-    // Logger::showBitboard(board);
+    Logger::showBitboard(board);
     IOEngine::readInputAndRecover(board);
+
+    // 这是一个模拟真实比赛到了第 30 步左右的残破盘面
+    board.blacks = 0x0000000000040100ULL;  // 黑皇后
+    board.whites = 0x0008100000000000ULL;  // 白皇后
+    board.arrows = 0x0761284A1248021FULL;  // 散落的箭矢
+    board.player = BitEngine::Player::BLACK;
     Logger::showBitboard(board);
 
     BitEngine::Move chosenMove;
@@ -64,6 +70,6 @@ int main() {
 
     BitEngine::applyMove(board, chosenMove);
     Logger::showBitboard(board);
-
+    printf("PassedTime: %d", Timer::getPassedTime());
     return 0;
 }
